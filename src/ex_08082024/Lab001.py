3ec42099ed87f630023d3e6e3fcf5f8e4e42a0ee
Sonali Saxena <sonalisaxenain@gmail.com>
2024-08-09 21:04:58 +0530

    Lab002 Added

---
+++ src/ex_08082024/Lab001.py
@@ -0,0 +1,5 @@
+# This is a comment
+
+print("Hello Sonali, from here starts your new Learning Phase and turning point of your life")
+print(2+2)
+print(2-2)
---
+++ src/ex_08082024/Lab002.py
@@ -0,0 +1,1 @@
+print("Hello, today I got a new ray of hope")
