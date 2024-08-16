5fcb1049f76c806bffd7224b2ab014495128e87a
Sonali Saxena <sonalisaxenain@gmail.com>
2024-08-16 22:34:10 +0530

    HW 2 tasks from 13th Aug

--- src/ex_13082024/HwTakeInput&PrintTable.py
+++ src/ex_13082024/HwTakeInput&PrintTable.py
@@ -1,9 +1,18 @@
 """
 WAP to take user input a num and print its table using String Format
 """
-num= int(input("Enter the number whose table you want to print"))
+table = int(input("Enter the number whose table you want to print"))
 
-print(num*1)
-print(num*2)
-print(num*3)
 
+
+print(type(table))
+print(f"{table}*1 = {table*1}")
+print(f"{table}*2 = {table*2}")
+print(f"{table}*3 = {table*3}")
+print(f"{table}*4 = {table*4}")
+print(f"{table}*5 = {table*5}")
+print(f"{table}*6 = {table*6}")
+print(f"{table}*7 = {table*7}")
+print(f"{table}*8 = {table*8}")
+print(f"{table}*9 = {table*9}")
+print(f"{table}*10 = {table*10}")
